416cc16d9832443fd2943d23f3ed0980230220ff
lorenacaoli <lorena.oiveira@acelerazg.com.br>
2022-08-24 20:38:14 -0300

    Initial commit

---
+++ src/test/java/TarefastodolistTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TarefastodolistTest {
+
+    @Test
+    void cadastrar() {
+    }
+
+    @Test
+    void remover() {
+    }
+
+    @Test
+    void mostrar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+}
